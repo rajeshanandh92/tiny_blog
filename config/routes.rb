af77bca3f62d19239fe4f7dbb4deb0f8044702d0
Rajesh Anandh <rajeshanandh92@live.com>
2014-10-15 13:36:39 +0530

    Devise - User -added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  devise_for :users
+  
   root 'home#index', :as => 'home'
 
   # The priority is based upon order of creation: first created -> highest priority.
