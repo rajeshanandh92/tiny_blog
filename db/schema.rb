6dba7cc13f92b7f137726d0e45ea3e60fd96eab5
Rajesh Anandh <rajeshanandh92@live.com>
2014-10-15 22:47:31 +0530

    Customized - Post Module

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141015092533) do
+ActiveRecord::Schema.define(version: 20141015151252) do
+
+  create_table "comments", force: true do |t|
+    t.integer  "post_id"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.text     "title"
