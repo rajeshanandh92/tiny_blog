dee19f265425c7668eaa08b2a502e2f0c4d8ea87
Rajesh Anandh <rajeshanandh92@live.com>
2014-10-15 14:57:55 +0530

    Post - controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141015075802) do
+ActiveRecord::Schema.define(version: 20141015092533) do
+
+  create_table "posts", force: true do |t|
+    t.text     "title"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
